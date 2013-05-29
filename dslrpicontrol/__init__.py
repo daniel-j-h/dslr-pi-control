13cfb65e7ab2caf47c2f2a5f7ac29dddcef92c74
Daniel J. Hofmann <daniel@trvx.org>
2013-05-29 23:52:37 +0200

    persistent logging

--- dslrpicontrol/__init__.py
+++ dslrpicontrol/__init__.py
@@ -5,8 +5,12 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 import dslrpicontrol.views
 import dslrpicontrol.errorhandlers
+
+if app.debug is not True:
+    import dslrpicontrol.loggers
 
 
 # vim: set tabstop=4 shiftwidth=4 expandtab:
---
+++ dslrpicontrol/loggers.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+import logging
+from logging import Formatter
+from logging.handlers import RotatingFileHandler
+
+from dslrpicontrol import app
+
+
+handler = RotatingFileHandler(
+        app.config.get('LOG_FILE', 'dslr-pi-control.log'),
+        maxBytes=1024 * 1024 * 10, backupCount=5)
+
+handler.setFormatter(Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+handler.setLevel(logging.WARNING)
+
+app.logger.addHandler(handler)
+
+
+# vim: set tabstop=4 shiftwidth=4 expandtab:
