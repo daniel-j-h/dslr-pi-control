7fc99bfc510f2809293c9bd9413c86d50c09077c
Daniel J. Hofmann <daniel@trvx.org>
2013-05-30 00:09:57 +0200

    split configuration management

--- dslrpicontrol/__init__.py
+++ dslrpicontrol/__init__.py
@@ -3,7 +3,10 @@
 from flask import Flask
 
 
-app = Flask(__name__)
+app = Flask(__name__, instance_relative_config=True)
+
+app.config.from_pyfile('development.cfg')
+#app.config.from_pyfile('production.cfg')
 
 
 import dslrpicontrol.views
--- runserver.py
+++ runserver.py
@@ -3,9 +3,7 @@
 
 from dslrpicontrol import app
 
-
-app.config['SECRET_KEY'] = 'your-secret-key'
-app.run(host='0.0.0.0', port=8080, debug=True)
+app.run(host='0.0.0.0', port=8080)
 
 
 # vim: set tabstop=4 shiftwidth=4 expandtab:
