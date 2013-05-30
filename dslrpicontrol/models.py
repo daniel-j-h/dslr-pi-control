c0bad20352fd761683af6bcf49cc32f44eff6fa3
Daniel J. Hofmann <daniel@trvx.org>
2013-05-30 11:04:40 +0200

    finished camera models, added reset (raspberry usb workaround)

--- dslrpicontrol/models.py
+++ dslrpicontrol/models.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import re
-from subprocess import check_output, CalledProcessError
+from subprocess import check_call, check_output, CalledProcessError
 from flask import flash
 
 from dslrpicontrol import app
@@ -19,18 +19,21 @@ def auto_detect():
         ret = prepared_call(['--auto-detect'])
     except (CalledProcessError, EnvironmentError) as e:
         app.logger.exception(e)
-        flash(u'Auto-detecting failed', 'danger')
+        flash(u'Auto-detection request failed', 'danger')
         return []
 
     # nothing detected
     if (len(ret) == 2): return []
 
-    # filter: separating line
-    ret = [x for x in ret if not x.startswith('---') and not x.endswith('---')]
+    # header
+    ret.pop(0)
+    # separating line
+    ret.pop(0)
+
     # map: transform to [(model, port), ...] from smth. like: ["model   port  ", ...]
-    ret = [re.search(r'^(?P<model>.+?)(\s+)(?P<port>.+$)', x).group('model', 'port') for x in ret]
+    ret = [re.search(r'^(?P<model>.+?)(\s+)(?P<port>.+$)', x.strip()).group('model', 'port') for x in ret]
     # map: transform to [{model=x[0], port=x[1]}, ...]
-    return [dict(model=x[0].strip(), port=x[1].strip()) for x in ret]
+    return [dict(model=x[0], port=x[1]) for x in ret]
 
 
 def abilities():
@@ -38,25 +41,75 @@ def abilities():
         ret = prepared_call(['--abilities'])
     except (CalledProcessError, EnvironmentError) as e:
         app.logger.exception(e)
-        flash(u'Checking abilities failed', 'danger')
+        flash(u'Ability request failed', 'danger')
         return []
 
+    # filter: empty lines
+    ret = filter(lambda x: not len(x) == 0, ret)
     # map: transform to [(feature, support), ...] from smth. like: ["feature  : support  ", ...]
-    ret = [re.search(r'^(?P<feature>.*?)(\s*:\s*)(?P<support>.*$)', x).group('feature', 'support') for x in ret]
+    ret = [re.search(r'^(?P<feature>.*?)(\s*:\s*)(?P<support>.*$)', x.strip()).group('feature', 'support') for x in ret]
     # map: transform to [{feature=x[0], suppport=x[1]}, ...]
-    return [dict(feature=x[0].strip(), support=x[1].strip()) for x in ret]
+    return [dict(feature=x[0], support=x[1]) for x in ret]
+
+
+def storage_info():
+    try:
+        ret = prepared_call(['--storage-info'])
+    except (CalledProcessError, EnvironmentError) as e:
+        app.logger.exception(e)
+        flash(u'Storage information request failed', 'danger')
+        return []
+
+    # filter: group name
+    ret = filter(lambda x: not x.startswith('[') and not x.endswith(']') and not len(x) == 0, ret)
+    # map: transform to (property, value) from smth. like: ["property=value", ...]
+    ret = [re.search(r'^(?P<property>.+?)(=)(?P<value>.+$)', x.strip()).group('property', 'value') for x in ret]
+    # map: transform to [{property=x[0], value=x[1]}, ...]
+    return [dict(property=x[0], value=x[1]) for x in ret]
+
+
+def summary():
+    try:
+        ret = prepared_call(['--summary'])
+    except (CalledProcessError, EnvironmentError) as e:
+        app.logger.exception(e)
+        flash(u'Summary request failed', 'danger')
+        return []
+
+    # filter: empty lines
+    ret = filter(lambda x: not len(x) == 0, ret)
+    # map: transform to [(property, value), ...] from smth. like: ["property:value  ", ...]
+    print(ret)
+    ret = [re.search(r'^(?P<property>.*?)(\s*:\s*)(?P<value>.*$)', x.strip()).group('property', 'value') for x in ret]
+    # map: transform to [{feature=x[0], suppport=x[1]}, ...]
+    return [dict(property=x[0], value=x[1]) for x in ret]
 
 
 def list_config():
     return prepared_call(['--list-config'])
 
 
-def storage_info():
-    return prepared_call(['--storage-info'])
+def reset_usb():
+    try:
+        ret = check_output(['lsusb']).splitlines()
+        ret = filter(lambda x: app.config.get('CAMERA', 'Nikon') in x, ret)
 
+        if len(ret) < 1:
+            app.logger.error('usbreset found no camera')
+            flash(u'USB reset request found no camera, not resetting', 'danger')
+            return
 
-def summary():
-    return prepared_call(['--summary'])
+        if len(ret) > 1:
+            app.logger.warning('usbreset found more than one camera')
+            flash(u'USB reset request found more than one camera, resetting all', 'info')
+
+        ret = [re.search(r'Bus (?P<bus>\d{3}?) Device (?P<device>\d{3})', x.strip()).group('bus', 'device') for x in ret]
+        map(lambda x: check_call(['usbreset', '/dev/bus/usb/{0}/{1}'.format(x[0], x[1])]), ret)
+
+        flash(u'USB reset request was successfull', 'success')
+    except (CalledProcessError, EnvironmentError) as e:
+        app.logger.exception(e)
+        flash(u'USB reset request failed', 'danger')
 
 
 # vim: set tabstop=4 shiftwidth=4 expandtab:
--- dslrpicontrol/views.py
+++ dslrpicontrol/views.py
@@ -4,7 +4,7 @@ from flask import flash, redirect, url_for
 
 from dslrpicontrol import app
 from dslrpicontrol.decorators import templated
-from dslrpicontrol.models import auto_detect, abilities
+from dslrpicontrol.models import auto_detect, abilities, storage_info, summary, reset_usb
 
 
 @app.route('/')
@@ -16,7 +16,12 @@ def index():
 @app.route('/camera')
 @templated('camera.html')
 def camera():
-    return dict(auto_detect=auto_detect(), abilities=abilities())
+    return dict(auto_detect=auto_detect(), abilities=abilities(), storage_info=storage_info(), summary=summary())
+
+@app.route('/camera/autodetect')
+@templated('cameraproperties.html')
+def camera_auto_detect():
+    return dict(caption='Auto detection', headers=['Key', 'Value'], properties=[dict(a='b', c='d'), dict(e='f', g='h')])
 
 
 @app.route('/capture')
@@ -31,6 +36,12 @@ def capture():
 def timelapse():
     flash(u'timelapse alert', 'info')
     return None
+
+
+@app.route('/reset')
+def reset():
+    reset_usb()
+    return redirect(url_for('timelapse'))
 
 
 # vim: set tabstop=4 shiftwidth=4 expandtab:
