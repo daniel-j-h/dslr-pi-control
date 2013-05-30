4e47f2d6b53ba2e3b17a76897c83d933f5f1328b
Daniel J. Hofmann <daniel@trvx.org>
2013-05-30 02:37:07 +0200

    base layout

--- utils/reset.py
+++ utils/reset.py
@@ -6,4 +6,4 @@ ret = os.popen('lsusb').read()
 for line in ret.split('\n') :
   if 'Nikon' not in line : continue
 
-  os.system("./usbreset /dev/bus/usb/%s/%s" % (line[4:7], line[15:18]))
+  os.system("usbreset /dev/bus/usb/%s/%s" % (line[4:7], line[15:18]))
