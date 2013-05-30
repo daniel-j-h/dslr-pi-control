ca74a890b2d0dda4436762058d67001ac6f5d670
Daniel J. Hofmann <daniel@trvx.org>
2013-05-30 03:30:36 +0200

    icons, alerts

--- dslrpicontrol/views.py
+++ dslrpicontrol/views.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from flask import render_template
+from flask import render_template, flash
 
 from dslrpicontrol import app
 from dslrpicontrol.decorators import templated
@@ -9,7 +9,29 @@ from dslrpicontrol.decorators import templated
 @app.route('/')
 @templated('baselayout.html')
 def index():
-    return dict(msg='Hello there!')
+    return dict(heading='pi-control', subtext='it\'s awesome!')
+
+
+@app.route('/camera')
+@templated('baselayout.html')
+def camera():
+    flash(u'camera alert', 'danger')
+    flash(u'2nd alert')
+    return None
+
+
+@app.route('/capture')
+@templated('baselayout.html')
+def capture():
+    flash(u'capture alert', 'success')
+    return None
+
+
+@app.route('/timelapse')
+@templated('baselayout.html')
+def timelapse():
+    flash(u'timelapse alert', 'info')
+    return None
 
 
 # vim: set tabstop=4 shiftwidth=4 expandtab:
